31dff59aed9279538604fc6053080e9bdf99c8b8
Yuri <hoc751@gmail.com>
2026-01-07 14:37:29 +0300

    test with avatars

--- src/pages/contact-page/contacts-page.tsx
+++ src/pages/contact-page/contacts-page.tsx
@@ -1,5 +1,5 @@
 import styles from "./contacts-page.module.css";
-import { Avatar } from "@mui/material";
+// import { Avatar } from "@mui/material";
 
 export const ContactsPage = () => {
   return (
@@ -7,21 +7,21 @@ export const ContactsPage = () => {
       <h2 className={styles.title}>Contact's</h2>
       <div className={styles.box}>
         <div className={styles.contacts}>
-          <Avatar
+          {/* <Avatar
             alt="kostykovich"
             src="/kostyk.jpg"
             sx={{ width: 400, height: 400 }}
-          />
+          /> */}
           <div className={styles.name}>Хитрый Еврей</div>
           <div className={styles.tg}>telegram: @Sergei9293</div>
         </div>
 
         <div className={styles.contacts}>
-          <Avatar
+          {/* <Avatar
             alt="svirydzenka"
             src="/svirydzenka.png"
             sx={{ width: 400, height: 400 }}
-          />
+          /> */}
           <div className={styles.name}>Умный Человек</div>
           <div className={styles.tg}>telegram: @noose92</div>
         </div>
