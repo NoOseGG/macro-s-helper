01bd31a5967023ed5379ead7753b6f8360fa2ec7
NoOse <hoc751@gmail.com>
2026-01-22 09:58:48 +0300

    added sign in

--- src/app/entry-point/main.tsx
+++ src/app/entry-point/main.tsx
@@ -3,9 +3,18 @@ import { createRoot } from "react-dom/client";
 import "../styles/reset.css";
 import "../styles/index.css";
 import { Router } from "../router/router";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router />
-  </StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <Router />
+    </ClerkProvider>
+  </StrictMode>,
 );
--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -4,6 +4,7 @@ import styles from "./home-page.module.css";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router";
 import { NavigatePath } from "../../shared/constants/constants";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
 
 export const HomePage: React.FC = () => {
   const navigate = useNavigate();
@@ -34,44 +35,58 @@ export const HomePage: React.FC = () => {
   return (
     <div className={styles.homePage}>
       <h1 className={styles.title}>Macro's Helper</h1>
-      <div className={styles.buttons}>
-        <Button
-          variant="outlined"
-          onClick={() => onNavigate(NavigatePath.Instruction)}
-        >
-          Инструкция по настройке
-        </Button>
-        <Button
-          variant="outlined"
-          onClick={() => onNavigate(NavigatePath.Button)}
-        >
-          Макрос на одну кнопку
-        </Button>
-        <Button
-          variant="outlined"
-          onClick={() => onNavigate(NavigatePath.SomeButtons)}
-        >
-          Макрос на несколько кнопкок
-        </Button>
-        <Button
-          variant="outlined"
-          onClick={() => onNavigate(NavigatePath.CloneMacros)}
-        >
-          Клонировать готовый макрос
-        </Button>
-        <Button
-          variant="outlined"
-          onClick={() => onNavigate(NavigatePath.SomeBats)}
-        >
-          Несколько батников
-        </Button>
-        <Button
-          variant="outlined"
-          onClick={() => onNavigate(NavigatePath.BuildMacros)}
-        >
-          Собрать макрос из своих
-        </Button>
-      </div>
+      <SignedOut>
+        <div className={styles.signInBox}>
+          <h3 className={styles.signInTitle}>
+            Вы не авторизированы, войдите в систему.
+          </h3>
+          <div className={styles.SignInButtonWrapper}>
+            <SignInButton mode="modal">
+              <button className={styles.SignInButton}>Авторизоваться</button>
+            </SignInButton>
+          </div>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <div className={styles.buttons}>
+          <Button
+            variant="outlined"
+            onClick={() => onNavigate(NavigatePath.Instruction)}
+          >
+            Инструкция по настройке
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => onNavigate(NavigatePath.Button)}
+          >
+            Макрос на одну кнопку
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => onNavigate(NavigatePath.SomeButtons)}
+          >
+            Макрос на несколько кнопкок
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => onNavigate(NavigatePath.CloneMacros)}
+          >
+            Клонировать готовый макрос
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => onNavigate(NavigatePath.SomeBats)}
+          >
+            Несколько батников
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => onNavigate(NavigatePath.BuildMacros)}
+          >
+            Собрать макрос из своих
+          </Button>
+        </div>
+      </SignedIn>
     </div>
   );
 };
